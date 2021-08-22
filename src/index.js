08b078456d9ea106d44c8e650f45e0954f04856d
Vladis Markin <vladismarkin@gmail.com>
2021-08-22 19:04:56 +0300

    Finish installing redux

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
-import axios from "axios";
+import reducers from "./reducers";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = "http://rem-rest-api.herokuapp.com/api";
 
+const store = createStore(reducers);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/reducers/index.js
@@ -0,0 +1,4 @@
+import { combineReducers } from "redux";
+import UsersReducer from "./users";
+
+export default combineReducers({ users: UsersReducer });
